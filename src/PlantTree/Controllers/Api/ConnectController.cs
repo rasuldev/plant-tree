ce3790d27a93da15d4e5c623271f3979b350b805
Rasul <rasuldev@gmail.com>
2017-04-25 15:50:45 +0300

    fixed namespaces error in views; changed type of expire_in

--- src/PlantTree/Controllers/Api/ConnectController.cs
+++ src/PlantTree/Controllers/Api/ConnectController.cs
@@ -28,7 +28,7 @@ namespace PlantTree.Controllers.Api
             public string scope { get; set; }
             public string token_type { get; set; }
             public string access_token { get; set; }
-            public string expires_in { get; set; }
+            public int expires_in { get; set; }
             public string refresh_token { get; set; }
             public string id_token { get; set; }
         }
